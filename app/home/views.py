c7fb32445f9fce9540c9ee5475814f1cc8cebea6
hlz <hlz@email.com>
2018-02-02 18:16:23 +0800

    flask 首页书写

--- app/home/views.py
+++ app/home/views.py
@@ -2,9 +2,9 @@ from . import home  # 从当前目录引入蓝图对象
 from flask import render_template, redirect, url_for
 
 
-@home.route('/')
-def index():
-    return render_template('home/index.html')
+# @home.route('/')
+# def index():
+#     return render_template('home/index.html')
 
 
 @home.route('/login/')
@@ -21,3 +21,36 @@ def logout():
 @home.route('/regist/')
 def regist():
     return render_template('home/regist.html')
+
+
+@home.route('/user')
+def user():
+    return render_template('home/user.html')
+
+
+@home.route('/pwd/')
+def pwd():
+    return render_template('home/pwd.html')
+
+
+@home.route('/comments/')
+def comments():
+    return render_template('home/comments.html')
+
+
+@home.route('/loginlog/')
+def loginlog():
+    return render_template('home/loginlog.html')
+
+
+@home.route('/moviecol/')
+def moviecol():
+    return render_template('home/moviecol.html')
+@home.route('/')
+def index():
+    return render_template('home/index.html')
+
+
+@home.route('/animation/')
+def animation():
+    return render_template('home/animation.html')
